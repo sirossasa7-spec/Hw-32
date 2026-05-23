892e88ff0833727c9dae5779525028b0d1dea311
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-23 19:34:53 +0200

    Main

--- Hw-32/Hw-32.cpp
+++ Hw-32/Hw-32.cpp
@@ -1,8 +1,36 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <vector>
+
+#include "Lion.h"
+#include "Wolf.h"
+#include "Dog.h"
+
+#include "Chicken.h"
+#include "Duck.h"
+#include "Eagle.h"
+
+using namespace std;
+
+int main() {
+
+    vector<Animals::Animal*> animals;
+
+    animals.push_back(new Animals::Wild::Lion());
+    animals.push_back(new Animals::Wild::Wolf());
+    animals.push_back(new Animals::Wild::Dog());
+
+    animals.push_back(new Birds::Domestic::Chicken());
+    animals.push_back(new Birds::Domestic::Duck());
+    animals.push_back(new Birds::Domestic::Eagle());
+
+    for (auto animal : animals) {
+        animal->makeSound();
+    }
+
+    for (auto animal : animals) {
+        delete animal;
+    }
+
+    return 0;
 }
-
-
